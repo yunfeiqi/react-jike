a2e89858407c1115c3a4d0752c87cd14004a411c
Your Name <you@example.com>
2024-04-22 21:51:27 +0800

    登录页静态结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,40 @@
-const Login = ()  =>{
-    return <div>This is login</div>
+import "./index.scss"
+import { Card, Form, Input, Button, message } from "antd"
+import logo from "@/assets/logo.png"
+
+
+const Login = () => {
+
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              { pattern: /^1[3-9]\d{9}$/, message: "手机号格式错误" }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
